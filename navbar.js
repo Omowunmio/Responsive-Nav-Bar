3761d558aeece4d6952090d49f4aa3a32f5bbfa3
wunmi <wunmieeplus@gmail.com>
2024-07-23 16:14:54 +0100

    first commit

---
+++ navbar.js
@@ -0,0 +1,13 @@
+const menuIcon = document.getElementById("menu-icon")
+const mobileMenu = document.getElementById("mobile-menu")
+const closeIcon = document.getElementById("close-icon")
+
+menuIcon.addEventListener("click", function(){
+    mobileMenu.style.display = "flex"
+})
+
+closeIcon.addEventListener("click", function(){
+    mobileMenu.style.display = "none"
+})
+
+closeIcon.addEventListener("")
